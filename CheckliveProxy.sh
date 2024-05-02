5baf2c1890ed3024d49bf2aeceffe0c368ee4bcb
lowji194 <theloi194@gmail.com>
2024-05-02 08:19:51 +0700

    Update CheckliveProxy.sh

--- CheckliveProxy.sh
+++ CheckliveProxy.sh
@@ -1,7 +1,7 @@
  proxyPort=$(head -n 1 /root/proxy.txt | cut -d ':' -f 1,2)
 
  Kiểm tra kết nối internet thông qua proxy
- if ! curl -x "$proxyPort" -m 20 -s api.myip.com &> /dev/null; then
+ if ! curl -x "$proxyPort" -m 60 -s api.myip.com &> /dev/null; then
     kill $(pgrep StartProxy)
 	echo "Kill StartProxy"
  fi
