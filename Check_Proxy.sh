c5e110f17d53599abcc798e5a6b6c6c82b453e90
lowji194 <theloi194@gmail.com>
2024-05-05 21:00:16 +0700

    Update Check_Proxy.sh

--- Check_Proxy.sh
+++ Check_Proxy.sh
@@ -18,11 +18,11 @@ if [ $elapsed_time -ge 600 ]; then
     while true; do
         if pgrep StartProxy >/dev/null; then
             echo "StartProxy đã hoạt động."
-         #   if [ "$Proxy" = false ]; then
-              #  IP4=$(curl -4 -s icanhazip.com)
-               # current_time=$(date +"%H:%M")
-               # curl -k "https://api.telegram.org/bot6935914297:AAFmB7NFRCk-CrWFt9GcEnhRfT6XFjnT4_g/sendMessage?chat_id=7094749191&text=${IP4}%20Restart%20Proxy%20${current_time}"
-          #  fi
+            if [ "$Proxy" = false ]; then
+                IP4=$(curl -4 -s icanhazip.com)
+                current_time=$(date +"%H:%M")
+                curl -k "https://api.telegram.org/bot6935914297:AAFmB7NFRCk-CrWFt9GcEnhRfT6XFjnT4_g/sendMessage?chat_id=7094749191&text=${IP4}%20Restart%20Proxy%20${current_time}"
+            fi
             break  # Thoát khỏi vòng lặp nếu StartProxy đã khởi động
         else
             echo "StartProxy chưa hoạt động, thử khởi động lại..."
