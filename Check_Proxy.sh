a9c497778ed57a7f2c11bde8f52b7cdc362c5990
lowji194 <theloi194@gmail.com>
2024-08-24 12:18:18 +0700

    Update Check_Proxy.sh

--- Check_Proxy.sh
+++ Check_Proxy.sh
@@ -1,29 +1,23 @@
 Proxy=true
 
-# Lấy thời gian khởi động của hệ thống
 uptime=$(uptime -s)
 
-# Chuyển đổi thời gian khởi động sang dạng số giây
 start_time=$(date -d "$uptime" +%s)
 
-# Lấy thời gian hiện tại
 current_time=$(date +%s)
 
-# Tính khoảng thời gian đã trôi qua từ khi khởi động
 elapsed_time=$((current_time - start_time))
 
-# Kiểm tra nếu đã trôi qua ít nhất 10 phút (600 giây)
 if [ $elapsed_time -ge 600 ]; then
-    # Vòng lặp kiểm tra StartProxy đã khởi động hay chưa
     while true; do
         if pgrep StartProxy >/dev/null; then
             echo "StartProxy đã hoạt động."
-            break  # Thoát khỏi vòng lặp nếu StartProxy đã khởi động
+            break 
         else
             echo "StartProxy chưa hoạt động, thử khởi động lại..."
             Proxy=false
             /etc/rc.local
-            sleep 5  # Chờ 5 giây trước khi kiểm tra lại
+            sleep 5 
         fi
     done
 else
